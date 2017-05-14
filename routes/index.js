f1b926707df34be764c00a863dd13fe6dfa8b94b
Sean Rose <sean1rose@gmail.com>
2017-05-14 00:17:06 -0700

    playing with templating

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,31 @@
+// ROUTES!
+
 const express = require('express');
 const router = express.Router();
 
+// RESPONSE METHODS:
+  // 1. send simple string/data
+// res.send('string goes here');
+  
+  // 2. send back json data
+// res.json({item: 'this is an obj that will be json stringified'});
+  
+  // 3. send back/render a template (using pug/jade templating language)
+// res.render()
+
 // Do work here
 router.get('/', (req, res) => {
-  console.log('FOO!');
+  console.log('root route...');
   const sean = { name: 'Sean', age: 50, cool: true};
   // res.json(sean);
   // res.send('Hey! It works!');
-  res.json(req.query);
+  // res.json(req.query);
+  
+  // need a template to render out
+  res.render('hello', {
+    name: 'sean',
+    dog: req.query.dog
+  });
 });
 
 router.get('/reverse/:name', (req,res) => {
