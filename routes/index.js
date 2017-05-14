803a67a233b193b04410b4de2d67f459b7769043
Sean Rose <sean1rose@gmail.com>
2017-05-13 23:39:13 -0700

    playing with routing

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,16 @@ const router = express.Router();
 
 // Do work here
 router.get('/', (req, res) => {
-  res.send('Hey! It works!');
+  console.log('FOO!');
+  const sean = { name: 'Sean', age: 50, cool: true};
+  // res.json(sean);
+  // res.send('Hey! It works!');
+  res.json(req.query);
+});
+
+router.get('/reverse/:name', (req,res) => {
+  const reverse = [...req.params.name].reverse().join('');
+  res.send('it works - ', reverse);
 });
 
 module.exports = router;
